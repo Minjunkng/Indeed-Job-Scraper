9b4a48e1444c3ee66a230659dcf238270547c44c
Minjun Kang <minjunkang369@gmail.com>
2023-12-21 15:07:16 -0500

    Retrieves full descriptions for one position

--- Main.py
+++ Main.py
@@ -10,5 +10,13 @@ with sync_playwright() as p:
         page.click("text=Find jobs")
         html = page.inner_html("#jobsearch-ViewjobPaneWrapper")
         soup = BeautifulSoup(html, "lxml")
-        for i in range(len(soup.find_all("div", class_ = "css-tvvxwd ecydgvn1"))):
-                print(soup.find_all("div", class_ = "css-tvvxwd ecydgvn1")[i].text)
+        job_title = soup.find("h2", class_ = "jobsearch-JobInfoHeader-title css-161nklr e1tiznh50").text.split(" - ")[0]
+        company_name = soup.find("span", class_ = "css-1cxc9zk e1wnkr790").text
+        company_location = soup.find("div", class_ = "css-1eomiun eu4oa1w0").text
+        yearly_salary = soup.find("div", class_ = "css-k5flys eu4oa1w0").text.split(" - ")[0]
+        job_type = soup.find("div", class_ = "css-k5flys eu4oa1w0").text.split(" - ")[1].lstrip()
+        print(job_title)
+        print(company_name)
+        print(company_location)
+        print(yearly_salary)
+        print(job_type)
